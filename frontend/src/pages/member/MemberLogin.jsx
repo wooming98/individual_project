83423c7e16f654136eaf132296ffc45b3bd1a498
anwoomin <askcw39@naver.com>
2024-07-29 17:49:12 +0900

    로그인 페이지 생성

--- frontend/src/pages/member/MemberLogin.jsx
+++ frontend/src/pages/member/MemberLogin.jsx
@@ -8,11 +8,11 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Link,
 } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
+import React, { useState } from "react";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export function MemberLogin() {
   const [id, setId] = useState("");
