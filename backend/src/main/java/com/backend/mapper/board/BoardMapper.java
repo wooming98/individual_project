802309b5de0e6453c15afc475bf0b8b668d437a1
anwoomin <askcw39@naver.com>
2024-07-31 12:00:23 +0900

    글 목록 리스트 순서 시간기준으로 정렬

--- backend/src/main/java/com/backend/mapper/board/BoardMapper.java
+++ backend/src/main/java/com/backend/mapper/board/BoardMapper.java
@@ -10,8 +10,9 @@ import java.util.List;
 @Mapper
 public interface BoardMapper {
     @Select("""
-            SELECT b.board_index, b.title, m.nick_name
+            SELECT b.board_index, b.title, b.inserted ,m.nick_name
             FROM board b join member m on b.member_index = m.member_index
+            ORDER BY b.inserted DESC
             """)
     List<Board> list();
 
