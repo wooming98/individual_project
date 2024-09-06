cc92bb76ad369b7d6189770250d45713012535ac
안우민 <askcw39@naver.com>
2024-09-07 01:01:21 +0900

    JWTUtil 작성

---
+++ backend/src/main/java/com/backend/security/JWTUtil.java
@@ -0,0 +1,53 @@
+package com.backend.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    // application.properties 파일에 설정된 것을 기반으로 생성
+    private SecretKey secretKey;
+
+    // application.properties 파일에 있는 spring.jwt.secret 값을 주입받아 SecretKey 객체 초기화
+    public JWTUtil(@Value("${jwt.secret.key}")String secret) {
+
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    // jwt에서 username 추출
+    public String getUsername(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    // jwt에서 role 추출
+    public String getRole(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    // jwt가 만료되었는지 여부 확인
+    public Boolean isExpired(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    // username, role, expiredMs을 받아 jwt 생성
+    public String createJwt(String username, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis())) // 토큰 발행 시간
+                .expiration(new Date(System.currentTimeMillis() + expiredMs)) // 토큰 소멸 시간
+                .signWith(secretKey)
+                .compact();
+    }
+}
