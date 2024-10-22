2958b639e92c58b9c44a6ccb948340a1f7420544
안우민 <askcw39@naver.com>
2024-10-22 20:03:27 +0900

    글 목록 페이징에서 마지막 페이지 부분만 나오게 ex) 43페이지까지만 있는데 41~50페이지까지 나오는거 막음

--- backend/src/main/java/com/backend/service/board/BoardService.java
+++ backend/src/main/java/com/backend/service/board/BoardService.java
@@ -29,6 +29,8 @@ public class BoardService {
         Integer leftPageNumber = (page - 1) / 10 * 10 + 1;
         // 10개의 버튼 중 마지막 번호
         Integer rightPageNumber = leftPageNumber + 9;
+        //
+        rightPageNumber = Math.min(rightPageNumber, lastPageNumber);
 
         pageInfo.put("currentPageNumber", page);
         pageInfo.put("lastPageNumber", lastPageNumber);
