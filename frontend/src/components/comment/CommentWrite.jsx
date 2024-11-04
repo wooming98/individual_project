6f9c738f6701f135bc6f1ce1986150ca3a8580f8
안우민 <askcw39@naver.com>
2024-11-04 18:09:48 +0900

    댓글 컴포넌트 작성란

---
+++ frontend/src/components/comment/CommentWrite.jsx
@@ -0,0 +1,28 @@
+import { Avatar, Box, Button, Flex, Textarea } from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+
+export function CommentWrite({ id }) {
+  const [comment, setComment] = useState("");
+
+  function handleSubmitComment() {
+    axios.post("/api/comment/add", { id, comment });
+  }
+
+  return (
+    <Box w="100%" margin={8}>
+      <Flex w="100%">
+        <Avatar w={37} h={37} />
+        <Textarea
+          ml={5}
+          placeholder="댓글을 입력하세요."
+          minHeight="100px"
+          onChange={(e) => setComment(e.target.value)}
+        />
+      </Flex>
+      <Flex justifyContent="flex-end" mt={3}>
+        <Button onClick={handleSubmitComment}>댓글 쓰기</Button>
+      </Flex>
+    </Box>
+  );
+}
