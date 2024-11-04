de6b05cf8e994b41203372153f7ceec2221f82b5
안우민 <askcw39@naver.com>
2024-11-04 18:12:01 +0900

    Comment 파일 생성

---
+++ backend/src/main/java/com/backend/domain/comment/Comment.java
@@ -0,0 +1,17 @@
+package com.backend.domain.comment;
+
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+public class Comment {
+    private Integer commentIndex;
+    private Integer boardIndex;
+    private Integer memberIndex;
+    private String comment;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate createDate;
+    private String nickName;
+}
