a094bdecf6fe07dfb3bd72a6d62caeb13f3a38ff
안우민 <askcw39@naver.com>
2024-11-05 18:02:37 +0900

    로그인 안되어있으면 댓글 작성 안되게

--- frontend/src/components/comment/CommentWrite.jsx
+++ frontend/src/components/comment/CommentWrite.jsx
@@ -1,6 +1,7 @@
 import { Avatar, Box, Button, Flex, Textarea } from "@chakra-ui/react";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import axios from "axios";
+import { LoginContext } from "../LoginProvider.jsx";
 
 export function CommentWrite({
   boardIndex,
@@ -9,6 +10,7 @@ export function CommentWrite({
   setIsProcessing,
 }) {
   const [comment, setComment] = useState("");
+  const account = useContext(LoginContext);
 
   function handleSubmitComment() {
     setIsProcessing(true);
@@ -23,16 +25,29 @@ export function CommentWrite({
 
   return (
     <Box margin={8}>
-      <Flex w="100%">
-        <Avatar w={37} h={37} />
-        <Textarea
-          ml={5}
-          placeholder="댓글을 입력하세요."
-          minHeight="100px"
-          value={comment}
-          onChange={(e) => setComment(e.target.value)}
-        />
-      </Flex>
+      {account.accessToken === null ? (
+        <Flex w="100%">
+          <Avatar w={43} h={43} />
+          <Textarea
+            ml={5}
+            placeholder="댓글을 쓰려면 로그인이 필요합니다."
+            minHeight="100px"
+            isDisabled={true}
+          />
+        </Flex>
+      ) : (
+        <Flex w="100%">
+          <Avatar w={43} h={43} />
+          <Textarea
+            ml={5}
+            placeholder="댓글을 입력하세요."
+            minHeight="100px"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </Flex>
+      )}
+
       <Flex justifyContent="flex-end" mt={3}>
         <Button
           isLoading={isProcessing}
